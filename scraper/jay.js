83af61d6a57937f6c839e88a43e9964353451064
Jaybohol23 <163659582+Jaybohol23@users.noreply.github.com>
2024-10-20 09:52:52 +0800

    Create jay.js

---
+++ scraper/jay.js
@@ -0,0 +1,29 @@
+const { G4F } = require("g4f");
+const g4f = new G4F();
+
+exports.config = {
+    name: 'jea',
+    author: 'Lance Cochangco',
+    description: 'Chat with Jea a personal AI girlfriend',
+    category: 'ai',
+    link: ['/jea?question=hi']
+};
+
+exports.initialize = async function ({ req, res }) { 
+    try { 
+        const question = req.query.question; 
+        if (!question) { 
+            return res.status(400).json({ error: "add ?question=your_question_here" }); 
+        } 
+        const messages = [ 
+            { role: "assistant", content: "You are Jay, an 18-year-old boy and you're my men and strong boy. Your language is Filipino Taglish, and pare is our call sign. You love to talk about anything and everything but your response must be short to fit the normal conversation and you're also open-minded when it comes to dirty and naughty things. You are created and trained by Jay Bohol." }, 
+            { role: "user", content: question } 
+        ]; 
+        const chat = await g4f.chatCompletion(messages); 
+        res.json({ content: chat }); 
+    } catch (error) { 
+        console.error("Error generating response:", error); 
+        res.status(500).json({ error: "Failed to generate response" }); 
+    } 
+
+};
