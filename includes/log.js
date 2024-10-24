97c580deb7e98d8bc7334ad3ba9e8a7fd3223ea9
ZCDSPH <toshchatbot@gmail.com>
2024-10-24 22:02:02 +0800

    Update log.js

--- includes/log.js
+++ includes/log.js
@@ -3,29 +3,15 @@ const kleur = require('kleur');
 
 const getTime = () => moment().tz('Asia/Manila').format('HH:mm:ss');
 
-// Create logger with bold + color
 const createLogger = (colorFn, prefix = '') => 
-    (text) => console.log(kleur.bold(colorFn(`[${getTime()}] [ Ajiro ]${prefix} » ${text}`)));
+    (text) => console.log(kleur.bold(colorFn(`[${getTime()}] [ ZCDS PH ]${prefix} » ${text}`)));
 
-// Create plain logger without timestamp and prefix
-const createPlainLogger = (colorFn) => 
-    (text) => console.log(kleur.bold(colorFn(text)));
-
-// Create error logger with [ Error ] prefix
-const createErrorLogger = (colorFn) => 
-    (text) => console.log(kleur.bold(colorFn(`[${getTime()}] [ Ajiro ] [ Error ] » ${text}`)));
-
-// Create warn logger with [ Warn ] prefix
-const createWarnLogger = (colorFn) => 
-    (text) => console.log(kleur.bold(colorFn(`[${getTime()}] [ Ajiro ] [ Warn ] » ${text}`)));
-
-// Set up the loggers using both bold and color
 const log = {
     main: createLogger(kleur.blue),
     hm: createLogger(kleur.blue),
-    plain: createPlainLogger(kleur.blue),
-    error: createErrorLogger(kleur.red),
-    warn: createWarnLogger(kleur.yellow)
+    plain: (text) => console.log(kleur.bold(kleur.blue(text))),
+    error: (text) => createLogger(kleur.red, ' [ Error ]')(text),
+    warn: (text) => createLogger(kleur.yellow, ' [ Warn ]')(text)
 };
 
 module.exports = log;
