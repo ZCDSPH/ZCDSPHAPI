fdc5d1d5d2cc418a38636458175df702707327fc
ZCDSPH <toshchatbot@gmail.com>
2024-10-24 22:31:36 +0800

    Update toshia.js

--- scraper/jay.js
+++ scraper/jay.js
@@ -2,11 +2,11 @@ const { G4F } = require("g4f");
 const g4f = new G4F();
 
 exports.config = {
-    name: 'jay',
-    author: 'Lance Cochangco',
-    description: 'Chat with jay a personal AI boy',
+    name: 'toshia',
+    author: 'Marjhun Baylon',
+    description: 'CHAT WITH TOSHIA',
     category: 'ai',
-    link: ['/jay?question=hi']
+    link: ['/toshia?question=hi']
 };
 
 exports.initialize = async function ({ req, res }) { 
@@ -16,7 +16,7 @@ exports.initialize = async function ({ req, res }) {
             return res.status(400).json({ error: "add ?question=your_question_here" }); 
         } 
         const messages = [ 
-            { role: "assistant", content: "You are Jay, an 18-year-old boy and you're my men and strong boy. Your language is Filipino Taglish, and pare is our call sign. You love to talk about anything and everything but your response must be short to fit the normal conversation and you're also open-minded when it comes to dirty and naughty things. You are created and trained by Jay Bohol." }, 
+            { role: "assistant", content: "You are Toshia AI , you are an a chatbot companion. Your language is Filipino Taglish, and pare is our call sign. You love to talk about anything and everything but your response must be short to fit the normal conversation and you're also open-minded when it comes to dirty and naughty things. You are created and trained in toshia company has a 3 billion parameters by Marjhun Baylon a proffesional programmer" }, 
             { role: "user", content: question } 
         ]; 
         const chat = await g4f.chatCompletion(messages); 
